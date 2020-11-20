ad5629188cd896a9c1383f0e1d643fd9f27aeecc
mikaelene <mikael.ene@eneanalytics.com>
2020-11-20 08:07:01 +0100

    Updated setup.py with correct version

--- dbt/include/sqlserver/macros/adapters.sql
+++ dbt/include/sqlserver/macros/adapters.sql
@@ -2,6 +2,15 @@
   information_schema
 {%- endmacro %}
 
+{% macro sqlserver__get_columns_in_query(select_sql) %}
+     {% call statement('get_columns_in_query', fetch_result=True, auto_begin=False) -%}
+         select TOP 0 * from (
+             {{ select_sql }}
+         ) as __dbt_sbq
+     {% endcall %}
+     {{ return(load_result('get_columns_in_query').table.columns | map(attribute='name') | list) }}
+ {% endmacro %}
+
 {% macro sqlserver__list_relations_without_caching(schema_relation) %}
   {% call statement('list_relations_without_caching', fetch_result=True) -%}
     select
@@ -38,25 +47,60 @@
   {% endcall %}
 {% endmacro %}
 
-{% macro sqlserver__drop_schema(database_name, schema_name) -%}
+
+{% macro sqlserver__drop_schema(relation) -%}
+  {%- set tables_in_schema_query %}
+      SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES
+      WHERE TABLE_SCHEMA = '{{ relation.schema }}'
+  {% endset %}
+  {% set tables_to_drop = run_query(tables_in_schema_query).columns[0].values() %}
+  {% for table in tables_to_drop %}
+    {%- set schema_relation = adapter.get_relation(database=relation.database,
+                                               schema=relation.schema,
+                                               identifier=table) -%}
+    {% do drop_relation(schema_relation) %}
+  {%- endfor %}
+
   {% call statement('drop_schema') -%}
-    drop schema if exists {{ relation.without_identifier().schema }}
+      IF EXISTS (SELECT * FROM sys.schemas WHERE name = '{{ relation.schema }}')
+      BEGIN
+      EXEC('DROP SCHEMA {{ relation.schema }}')
+      END
   {% endcall %}
 {% endmacro %}
 
+{# TODO make this function just a wrapper of sqlserver__drop_relation_script #}
 {% macro sqlserver__drop_relation(relation) -%}
+  {% if relation.type == 'view' -%}
+   {% set object_id_type = 'V' %}
+   {% elif relation.type == 'table'%}
+   {% set object_id_type = 'U' %}
+   {%- else -%} invalid target name
+   {% endif %}
   {% call statement('drop_relation', auto_begin=False) -%}
-    drop {{ relation.type }} if exists {{ relation.schema }}.{{ relation.identifier }}
+    if object_id ('{{ relation.include(database=False) }}','{{ object_id_type }}') is not null
+      begin
+      drop {{ relation.type }} {{ relation.include(database=False) }}
+      end
   {%- endcall %}
 {% endmacro %}
 
 {% macro sqlserver__drop_relation_script(relation) -%}
-    drop {{ relation.type }} if exists {{ relation.schema }}.{{ relation.identifier }}
+  {% if relation.type == 'view' -%}
+   {% set object_id_type = 'V' %}
+   {% elif relation.type == 'table'%}
+   {% set object_id_type = 'U' %}
+   {%- else -%} invalid target name
+   {% endif %}
+  if object_id ('{{ relation.include(database=False) }}','{{ object_id_type }}') is not null
+      begin
+      drop {{ relation.type }} {{ relation.include(database=False) }}
+      end
 {% endmacro %}
 
-{% macro sqlserver__check_schema_exists(database, schema) -%}
+
+{% macro sqlserver__check_schema_exists(information_schema, schema) -%}
   {% call statement('check_schema_exists', fetch_result=True, auto_begin=False) -%}
-    --USE {{ database_name }}
     SELECT count(*) as schema_exist FROM sys.schemas WHERE name = '{{ schema }}'
   {%- endcall %}
   {{ return(load_result('check_schema_exists').table) }}
@@ -162,6 +206,10 @@
   {% set table = load_result('get_columns_in_relation').table %}
   {{ return(sql_convert_columns_in_relation(table)) }}
 {% endmacro %}
+
+
+
+
 
 {% macro sqlserver__make_temp_relation(base_relation, suffix) %}
     {% set tmp_identifier = '#' ~  base_relation.identifier ~ suffix %}
